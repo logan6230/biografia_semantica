157d7d517db51a50bec14fa049a2cc3e65872f8e
logan6230 <pantoja2187@gmail.com>
2023-03-14 08:18:36 -0500

    Crear formularios y manejo de eventos

---
+++ formularios.js
@@ -0,0 +1,6 @@
+function mostrarMensaje() {
+    console.log('Hicieron click en el boton', new Date().toLocaleTimeString());
+}
+//Agregar un eventListener
+let boton = document.getElementById('btn');
+boton.addEventListener("click", mostrarMensaje)
